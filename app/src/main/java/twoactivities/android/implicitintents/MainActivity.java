67a363b3a0290df949847ae3f283142fab52b7e7
Keldrick <kelridgemitchelle@kelridges-mbp.home>
2019-06-26 16:57:03 -0500

    Initial Commit

---
+++ app/src/main/java/twoactivities/android/implicitintents/MainActivity.java
@@ -0,0 +1,57 @@
+package twoactivities.android.implicitintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+
+            startActivity(intent);
+
+
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
+
+    }
+
+    public void openLocation(View view) {
+        mLocationEditText = findViewById(R.id.location_edittext);
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+
+
+    }
+}
